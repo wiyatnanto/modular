2a68e98ff47aa6ddf203d1cd047ae421f8a038a4
Eko Wiyatnanto <wiyatnanto@gmail.com>
2024-03-13 02:10:04 +0700

    update laravel 11 compatibility

--- src/Console/Commands/Make/MakeLivewire.php
+++ src/Console/Commands/Make/MakeLivewire.php
@@ -6,9 +6,8 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
-use Livewire\Commands\MakeCommand;
+use Livewire\Features\SupportConsoleCommands\Commands\MakeCommand;
 use Livewire\Livewire;
-use Livewire\LivewireComponentsFinder;
 
 if (class_exists(MakeCommand::class)) {
     class MakeLivewire extends MakeCommand
@@ -23,7 +22,8 @@ if (class_exists(MakeCommand::class)) {
         public function handle()
         {
             if ($module = $this->module()) {
-                Config::set('livewire.class_namespace', $module->qualify('Http\\Livewire'));
+
+                Config::set('livewire.class_namespace', $module->qualify(str_replace('App\\', '', config('livewire.class_namespace'))));
                 Config::set('livewire.view_path', $module->path('resources/views/livewire'));
 
                 $app = $this->getLaravel();
@@ -32,13 +32,15 @@ if (class_exists(MakeCommand::class)) {
                 ? '/tmp/storage/bootstrap/cache/livewire-components.php'
                 : $app->bootstrapPath('cache/livewire-components.php');
 
-                $componentsFinder = new LivewireComponentsFinder(
-                    new Filesystem(),
-                    Config::get('livewire.manifest_path') ?? $defaultManifestPath,
-                    $module->path('src/Http/Livewire')
-                );
+                if (class_exists("Livewire\LivewireComponentsFinder")) {
+                    $componentsFinder = new \Livewire\LivewireComponentsFinder(
+                        new Filesystem(),
+                        Config::get('livewire.manifest_path') ?? $defaultManifestPath,
+                        $module->path('src/Http/Livewire')
+                    );
 
-                $app->instance(LivewireComponentsFinder::class, $componentsFinder);
+                    $app->instance(\Livewire\LivewireComponentsFinder::class, $componentsFinder);
+                }
             }
 
             parent::handle();
@@ -47,13 +49,14 @@ if (class_exists(MakeCommand::class)) {
         protected function createClass($force = false, $inline = false)
         {
             if ($module = $this->module()) {
+
                 $name = Str::of($this->argument('name'))
                     ->split('/[.\/(\\\\)]+/')
                     ->map([Str::class, 'studly'])
                     ->join(DIRECTORY_SEPARATOR);
 
-                $classPath = $module->path('src/Http/Livewire/' . $name . '.php');
-
+                $classPath = $module->path('src/Livewire/' . $name . '.php');
+                
                 if (File::exists($classPath) && !$force) {
                     $this->line("<options=bold,reverse;fg=red> WHOOPS-IE-TOOTLES </> 😳 \n");
                     $this->line("<fg=red;options=bold>Class already exists:</> {$this->parser->relativeClassPath()}");
@@ -72,13 +75,12 @@ if (class_exists(MakeCommand::class)) {
                     ->implode('.');
 
                 $fully_qualified_component = Str::of($this->argument('name'))
-                    ->prepend('Http/Livewire/')
+                    ->prepend('Livewire/')
                     ->split('/[.\/(\\\\)]+/')
                     ->map([Str::class, 'studly'])
                     ->join('\\');
 
                 Livewire::component("{$module->name}::{$component_name}", $module->qualify($fully_qualified_component));
-
                 return $classPath;
             }
 
